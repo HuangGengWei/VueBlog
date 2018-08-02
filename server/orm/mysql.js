bc43f41c9ee2f8dc3403264075bb576e4c6b8d36
hgw <774921756@qq.com>
2018-08-02 23:38:46 +0800

    debug

--- server/orm/mysql.js
+++ server/orm/mysql.js
@@ -7,12 +7,12 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool(models.mysql);
 
-var query=function(sql,options,callback){
+var query=function(sql,callback){
   pool.getConnection(function(err,conn){
     if(err){
       callback(err,null,null);
     }else{
-      conn.query(sql,options,function(err,results,fields){
+      conn.query(sql,function(err,results,fields){
         //释放连接
         conn.release();
         //事件驱动回调
