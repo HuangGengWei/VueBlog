237a2cd0dba3e1d1ac1961c463f7e48251eb0f67
hgw <774921756@qq.com>
2018-08-15 00:02:13 +0800

    清除console.log

--- server/api/ApiClient.js
+++ server/api/ApiClient.js
@@ -4,12 +4,11 @@ let OrmClient = require('../orm/mongo/models/ClientModel');
 let ToolFunction = require('../tool/ToolFunction');
 
 router.post('/AddClientIP',(req,res)=>{
-  console.log('AddClientIP');
   //执行记录ip的代码-----------------------------------------------------------------------------
   let ip = OrmClient.getClientIP(req);
   //if (ip!='127.0.0.1'){
     OrmClient.getIpInfo('120.78.66.123').then(rst=>{
-      console.log('rst',rst);
+      //console.log('rst',rst);
       if (rst.STS=='OK'){
         let ipinfo = rst.data;
         let {ip,country,region,city,isp} = ipinfo;
