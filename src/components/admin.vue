e496a7e5840c05d90ede172e8fde1d1249f89079
hgw <774921756@qq.com>
2018-08-26 17:34:01 +0800

    debug

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -344,21 +344,23 @@
           $(this).addClass("active");
         });
       })
+
+      $(window).resize(function() {
+        //获取浏览器窗口高度
+        var winHeight=0;
+        if (window.innerHeight)
+          winHeight = window.innerHeight;
+        else if ((document.body) && (document.body.clientHeight))
+          winHeight = document.body.clientHeight;
+        //通过深入Document内部对body进行检测，获取浏览器窗口高度
+        if (document.documentElement && document.documentElement.clientHeight)
+          winHeight = document.documentElement.clientHeight;
+        //DIV高度为浏览器窗口的高度
+        document.getElementById("app").style.height= winHeight +"px";
+        //DIV高度为浏览器窗口高度的一半
+        //document.getElementById("app").style.height= winHeight/2 +"px";
+      });
     },
   }
-  $(window).resize(function() {
-    //获取浏览器窗口高度
-    var winHeight=0;
-    if (window.innerHeight)
-      winHeight = window.innerHeight;
-    else if ((document.body) && (document.body.clientHeight))
-      winHeight = document.body.clientHeight;
-    //通过深入Document内部对body进行检测，获取浏览器窗口高度
-    if (document.documentElement && document.documentElement.clientHeight)
-      winHeight = document.documentElement.clientHeight;
-    //DIV高度为浏览器窗口的高度
-    document.getElementById("app").style.height= winHeight +"px";
-    //DIV高度为浏览器窗口高度的一半
-    //document.getElementById("app").style.height= winHeight/2 +"px";
-  });
+
 </script>
