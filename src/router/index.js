9343143b67ffac9b6a95846d0840c79231ee13ad
hgw <774921756@qq.com>
2018-09-14 17:44:25 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import admin from '@/components/admin'
-import blogmgr from '@/components/blogmgr'
-import addblog from '@/components/addblog'
-import login from '@/components/login'
-import profile from '@/components/profile'
-import index from '@/components/index'
-import view from '@/components/view'
-import bloglist from '@/components/bloglist'
-import aboutme from '@/components/aboutme'
-import image from '@/components/image'
+
+// import admin from '@/components/admin'
+const admin = r => require.ensure([],()=>r(require('@/components/admin')),'admin')
+
+// import blogmgr from '@/components/blogmgr'
+const blogmgr = r => require.ensure([],()=>r(require('@/components/blogmgr')),'blogmgr')
+
+// import addblog from '@/components/addblog'
+const addblog = r => require.ensure([],()=>r(require('@/components/addblog')),'addblog')
+
+//import login from '@/components/login'
+const login = r => require.ensure([],()=>r(require('@/components/login')),'login')
+
+// import profile from '@/components/profile'
+const profile = r => require.ensure([],()=>r(require('@/components/profile')),'profile')
+
+// import index from '@/components/index'
+const index = r => require.ensure([],()=>r(require('@/components/index')),'index')
+
+//import view from '@/components/view'
+const view = r => require.ensure([],()=>r(require('@/components/view')),'view')
+
+//import bloglist from '@/components/bloglist'
+const bloglist = r => require.ensure([],()=>r(require('@/components/bloglist')),'bloglist')
+
+//import aboutme from '@/components/aboutme'
+const aboutme = r => require.ensure([],()=>r(require('@/components/aboutme')),'aboutme')
+
+// import image from '@/components/image'
+const image = r => require.ensure([],()=>r(require('@/components/image')),'image')
 Vue.use(Router)
 
 export default new Router({
